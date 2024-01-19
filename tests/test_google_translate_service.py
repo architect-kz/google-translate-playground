13fc54aa44c824fbcf3664183154fb4846f2ea35
Ra1ski <ra1almaty@gmail.com>
2024-01-19 12:55:05 +0600

    tests: Added test for GoogleTranslateService

---
+++ tests/test_google_translate_service.py
@@ -0,0 +1,41 @@
+import pytest
+
+from aiogoogletrans.models import Translated
+from unittest.mock import AsyncMock, patch
+
+from app.v1.services.google_translate import GoogleTranslateService
+from app.v1.models import Word, Language
+from tests.stubs.extra_data import extra_data
+
+
+@pytest.fixture
+def mock_translator():
+    with patch('aiogoogletrans.Translator') as mock:
+        yield mock
+
+
+@pytest.mark.asyncio
+async def test_get_translated_word(mock_translator):
+    params = {
+        'src': 'en',
+        'dest': 'es',
+        'word': 'challenge',
+        'text': 'desafío',
+    }
+    # Setup mock return value
+    translated_data = Translated(
+        src=params['src'], dest=params['dest'], origin=params['word'], text=params['text'],
+        extra_data=extra_data, pronunciation=None
+    )
+    mock_translator.return_value.translate = AsyncMock(return_value=translated_data)
+
+    service = GoogleTranslateService()
+
+    word_model = await service.get_translated_word('challenge', 'en', 'es')
+
+    assert word_model.word == params['word']
+    assert word_model.language == params['src']
+    assert isinstance(word_model.languages, dict)
+    assert isinstance(word_model, Word)
+    assert isinstance(word_model.languages[params['dest']], Language)
+    # ... more assertions based on your requirements
